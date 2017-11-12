23b05ed759c5f867cb54fc181984feafcf198aef
valentyn.melnyk <roelov@ukr.net>
2017-11-12 20:16:32 +0200

    12.11.2017 totum

--- js/common.js
+++ js/common.js
@@ -31,3 +31,62 @@ $(document).ready( function() {
          $("#filename").val(filename);
     });
 });
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+    "date-uk-pre": function ( a ) {
+        if (a == null || a == "") {
+            return 0;
+        }
+        var ukDatea = a.split('/');
+        return (ukDatea[2] + ukDatea[1] + ukDatea[0]) * 1;
+    },
+
+    "date-uk-asc": function ( a, b ) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "date-uk-desc": function ( a, b ) {
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
+
+$(document).ready(function() {
+    $('#example').DataTable({
+        "bSort": false,
+        "oLanguage": {
+            "oPaginate": {
+            "sFirst": "First page", // This is the link to the first page
+            "sPrevious": "Previous page", // This is the link to the previous page
+            "sNext": "Load more", // This is the link to the next page
+            "sLast": "Last page" // This is the link to the last page
+            }
+        }
+    } );
+} );
+
+$(document).ready(function() {
+    $(".tab_item").not(":first").hide();
+    $(".change_balance_tab_wrapper .tab").click(function() {
+        $(".change_balance_tab_wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+} );
+
+$(document).ready(function() {
+    var carousel = $("#carousel");
+    carousel.owlCarousel({
+        items: 6,
+    });
+    $('#js-prev').click(function () {
+        carousel.trigger('owl.prev');
+        return false;
+        });
+
+    $('#js-next').click(function () {
+        carousel.trigger('owl.next');
+
+        return false;
+    });
+} );
+
+
+
